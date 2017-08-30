e325106c3f0671dc2d498c0205c783f0c959201a
Arpit Jasapara <lordtipra@gmail.com>
2017-08-30 15:25:37 -0700

    Framework for Volunteer App

---
+++ Volunteer App/Volunteer App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Volunteer App
+//
+//  Created by Arpit Jasapara on 8/30/17.
+//  Copyright © 2017 Arpit Jasapara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
