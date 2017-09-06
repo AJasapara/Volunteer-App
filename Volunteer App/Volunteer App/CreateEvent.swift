6441a5de951f4a24a9d6d234b989ac82259c5e74
Arpit Jasapara <lordtipra@gmail.com>
2017-09-06 16:48:12 -0700

    Added image picking functionality

--- Volunteer App/Volunteer App/CreateEvent.swift
+++ Volunteer App/Volunteer App/CreateEvent.swift
@@ -12,10 +12,35 @@ import UIKit
 import CoreData
 import EasyCoreData
 
-class CreateEvent:  UIViewController {
+class CreateEvent:  UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var EventTitle: UITextField!
     @IBOutlet weak var Description: UITextField!
     @IBOutlet weak var NumVolunteers: UITextField!
     @IBOutlet weak var ZipCode: UITextField!
+    @IBOutlet weak var Photo: UIImageView!
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        Photo.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func pickPhoto(_ sender: UITapGestureRecognizer) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = f
+        present(imagePickerController, animated: true, completion: nil)
+    }
     
 }
