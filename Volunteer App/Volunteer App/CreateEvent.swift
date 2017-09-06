e31fea4c2ac56726db45710c232c0796d0fe3998
Arpit Jasapara <lordtipra@gmail.com>
2017-09-06 16:01:03 -0700

    Added Descriptors to Create Event View

---
+++ Volunteer App/Volunteer App/CreateEvent.swift
@@ -0,0 +1,21 @@
+//
+//  CreateEvent.swift
+//  Volunteer App
+//
+//  Created by Arpit Jasapara on 9/6/17.
+//  Copyright © 2017 Ed Basurto. All rights reserved.
+//
+
+
+
+import UIKit
+import CoreData
+import EasyCoreData
+
+class CreateEvent:  UIViewController {
+    @IBOutlet weak var EventTitle: UITextField!
+    @IBOutlet weak var Description: UITextField!
+    @IBOutlet weak var NumVolunteers: UITextField!
+    @IBOutlet weak var ZipCode: UITextField!
+    
+}
